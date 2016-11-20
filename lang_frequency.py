61cb5001602cf6b310b8ce7e53d587eca6c06ce7
Kazauwa <i.heart.d.dead@gmail.com>
2016-11-21 00:50:38 +0300

    done

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,23 @@
+from collections import Counter
+import re
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as reader:
+        raw_data = reader.readlines()
+    text = str()
+    for line in raw_data:
+        text += line.lower()
+    return text.replace('\n', '')
 
 
 def get_most_frequent_words(text):
-    pass
+    counter = Counter(re.findall('\w+', text))
+    return counter.most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    text = load_data(input('Path to the text file: '))
+    print('Up to 10 top words in given text:')
+    for word, frequency in get_most_frequent_words(text):
+        print('{0}: {1}'.format(word, frequency))
