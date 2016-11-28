23ea38d5c7a34dcdeafed046c9ee559205fa65d3
Kazauwa <i.heart.d.dead@gmail.com>
2016-11-28 12:42:47 +0300

    Magic constant replace, breaked load_data()

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,24 +1,32 @@
 from collections import Counter
+import sys
 import re
 
 
 def load_data(filepath):
     with open(filepath, 'r') as reader:
-        raw_data = reader.readlines()
-    text = str()
-    for line in raw_data:
-        text += line.lower()
-    return text.replace('\n', '')
+        raw_data = reader.read()
+    return raw_data
 
 
-def get_most_frequent_words(text):
-    counter = Counter(re.findall('\w+', text))
-    return counter.most_common(10)
+def prepare_data(text):
+    if not text:
+        print('Given file is empty!')
+        sys.exit(0)
+    words = re.findall('\w+', text)
+    cooked_words = [word.lower() for word in words]
+    return cooked_words
+
+
+def get_most_frequent_words(words_list, top_frequent=10):
+    counter = Counter(words_list)
+    return counter.most_common(top_frequent)
 
 
 if __name__ == '__main__':
     text = load_data(input('Path to the text file: '))
-    print('Up to 10 top words in given text:')
-    for word, frequency in get_most_frequent_words(text):
-        print('{0}: {1}'.format(word, frequency))
-
+    words_list = prepare_data(text)
+    top_words = get_most_frequent_words(words_list)
+    print('Up to 10 top words in given text:\n')
+    for word, frequency in top_words:
+        print('\'{0}\', {1} occurances'.format(word, frequency))
